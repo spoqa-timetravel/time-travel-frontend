ae2972fd6fc65d04cf7306729d3bfc02c674e19f
Yoonsu Kang <kangys0725@naver.com>
2019-05-18 22:59:27 +0900

    index page random number 추가

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".data").each(function() {
+    var n = $(this).text();
+    n <= 20 ? (z = 99) : (z = 0);
+    $(this)
+      .prop("Counter", z)
+      .animate(
+        {
+          Counter: n.replace(/,/g, "")
+        },
+        {
+          duration: 1000,
+          easing: "swing",
+          step: function(now) {
+            $(this).text(Math.ceil(now));
+          },
+          complete: function() {
+            $(this).text(n);
+          }
+        }
+      );
+  });
+  
