cf3a428e04842cdd90a3ac37b571c9bfd1a5a04d
boxfox619 <boxfoxsg619@gmail.com>
2019-05-19 04:42:37 +0900

    Finish implement the youtube url formatter

--- result/script.js
+++ result/script.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
+    $('#youtube').attr('src', 'https://www.youtube.com/embed/NIld_iEc67s');
     $.get("/post", function (data) {
         if (!data.image) { $('#image').css('display', 'none'); }
         if (!data.youtube) { $('#youtube').css('display', 'none'); }
         $('#date').text(data.date);
-        $('#youtube').attr('src', data.youtube);
+        $('#youtube').attr('src', formatYoutubeUrl(data.youtube));
         $('#image').attr('src', data.image);
         $('#caption').text(data.caption);
     });
@@ -12,3 +13,16 @@ $(document).ready(() => {
         window.location.reload();
     });
 });
+
+function formatYoutubeUrl(url) {
+    let token;
+    if (url.startsWith('https://youtu.be/')) {
+        token = url.replace('https://youtu.be/', '');
+    } else {
+        token = url.substring(url.indexOf('v=') + 2, url.length);
+    }
+	if(token.includes('&')) {
+    	token = token.substring(0, token.indexOf('&'));
+    }
+    return `https://www.youtube.com/embed/${token}`;
+}
